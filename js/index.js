3b6890cd3344682a5dcdeb14ed91f3b076645b40
qinshiju0422 <qinshiju0422@sina.com>
2017-05-15 19:41:49 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,115 @@
+var data=[
+	{
+		soung:"你就是我想要的丫头",
+		singer:"赵照",
+		id:"id",
+		about:[
+			{
+			share:"分享",
+			delete:"删除"
+			}
+		]
+	},
+	{
+		soung:"盛夏的果实",
+		singer:"莫文蔚",
+		id:"id",
+		about:[
+			{
+			share:"分享",
+			delete:"删除"
+			}
+		]
+	},
+	{
+		soung:"成都",
+		singer:"赵雷",
+		id:"id",
+		about:[
+			{
+			share:"分享",
+			delete:"删除"
+			}
+		]
+	}
+]
+document.addEventListener('DOMContentLoaded',function(){
+	function createLi(arr){
+		var oUl=document.querySelector('.sound');
+		// console.log(arr.length)
+		for (var i=0; i<arr.length; i++){
+			var aLi=document.createElement('li');
+				aLi.innerHTML='<a href="music.html" title="" class="song-singer">'+
+				'<span class="song">'+arr[i].soung+'</span>'+
+				'<span  class="singer">'+arr[i].singer+'</span>'+
+			'</a>'+
+			'<div class="shar-del clearFix">'+
+				'<a href="music.html" title="">'+
+						'<span class="share fl">'+arr[i].about[0].share+'</span>'+
+					'</a>'+
+				'<span class="delete fr">'+arr[i].about[0].delete+'</span>'+
+			'</div>'+
+			'<div class="my-look">'+
+				'<input type="button" name="" value="自己可见">'+
+			'</div>'
+			oUl.appendChild(aLi)
+			
+		}
+		var aShare=document.querySelectorAll('.share');
+		var aDelete=document.querySelectorAll('.delete');
+		var aLi=document.querySelectorAll('li');
+		var aMyLook=document.querySelectorAll('.my-look');
+		console.log(aMyLook.length)
+		var x=0;
+		for (var i=0; i<aDelete.length; i++){
+			aDelete[i].index=i;
+			aDelete[i].addEventListener('click',function(){
+				oUl.removeChild(aLi[this.index])
+			},false)
+		}
+		for (var i=0; i<aLi.length; i++){
+			aLi[i].indexT=i;
+			aLi[i].addEventListener('touchstart',function(e){
+				var startX=e.targetTouches[0].pageX-x;
+				function touchMove(e){
+					console.log(e)
+					x=e.targetTouches[0].pageX-startX;
+					console.log('左'+x+'右'+x)
+					console.log(aMyLook[this.indexT])
+					// console.log(aLi[this.index].index)
+					// aLi[this.index].style.width=x+'rem'
+					if (x<0) {
+						aLi[this.indexT].style.width=20.225+'rem';
+						aLi[this.indexT].style.left=-3+'rem';
+						x=0
+					}else{
+						aLi[this.indexT].style.width=17.225+'rem';
+						aLi[this.indexT].style.left=0+'rem';
+						x=0;
+					}
+					console.log('现在的值'+x)
+				};
+				function touchEnd(){
+					aLi[this.indexT].removeEventListener('touchmove',touchMove,false)
+					aLi[this.indexT].removeEventListener('touchend',touchEnd,false)
+				};
+				aLi[this.indexT].addEventListener('touchmove',touchMove,false);
+				aLi[this.indexT].addEventListener('touchend',touchEnd,false);
+				//e.preventDefault();
+			},false)
+		}
+	}
+	createLi(data)	
+},false)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/music.js
@@ -0,0 +1,133 @@
+document.addEventListener('DOMContentLoaded',function(){
+	var oPlay=document.querySelector('.play');
+	var oAudio=document.querySelector('audio');
+	var oStart=document.querySelector('.start');
+	var oStartImg=document.querySelector('.start-img');
+	var play=true;
+	var controler=null;
+	var controlerBar=null;
+	var oToTal=document.querySelector('.total')
+	var oBar=document.querySelector('.bar');
+	var oHadPlay=document.querySelector('.had-play');
+	var hadTimer=document.querySelector('#had-timer');
+	var allTimer=document.querySelector('#all-timer');
+	var oMusic=document.querySelector('.music');
+	var oLove=document.querySelector('.love');
+	var oCountMusic=document.querySelector('.count-music');
+	var oCountLove=document.querySelector('.count-love');
+	var x=0;
+	var n=1;
+	var aboveX=0;
+	var numDragpaddingLeft=0;
+	var num=1;
+	var y=1;
+	oMusic.src='images/rmusic.png'
+	oCountMusic.innerHTML=n++;
+	var oNav=document.querySelector('.nav');
+	oNav.addEventListener('click',function(){
+		oNav.style.display='none';
+	},false)
+	oLove.addEventListener('click',function(arr){
+		if(num){
+			oCountLove.innerHTML=y;
+			oLove.src='images/rLove.png';
+			num=0;
+			console.log(y)
+		}else{
+			if (num==0) {
+				y=0;
+				oCountLove.innerHTML=y;
+				oLove.src='images/love.png';
+				num=1;
+				y=1;
+			}
+		}
+	},false)
+	oStart.addEventListener('click',function(){
+		if (play) {
+			oAudio.play()
+			oStartImg.src='images/play.png';
+			play=false;
+			// clearInterval(controler);
+			// clearInterval(controlerBar)
+			controler=setInterval(time,1000);
+			controlerBar=setInterval(progress,1000);
+		}else{
+			oAudio.pause();
+			// clearInterval(controler);
+			// clearInterval(controlerBar)
+			oStartImg.src='images/stop.png';
+			play=true;	
+		}
+	},false)
+	function cutTime(time){
+	    var value = (time > 10 ? time + '' : '0' + time).substring(0, 2);
+	    return isNaN(value) ? '00' : value;
+    };
+
+	function time(){
+		var allTime = oAudio.duration,
+			hadTime = oAudio.currentTime;
+		hadTimer.innerHTML=''+cutTime(hadTime / 60)+':'+ cutTime(hadTime % 60)+'';
+		allTimer.innerHTML=''+cutTime(allTime / 60)+':'+cutTime(allTime % 60)+'';
+		return hadTimer.innerHTML,allTimer.innerHTML
+	}
+	function progress(){
+		var cuT = oAudio.currentTime,
+			toT = oAudio.duration;
+		var oToTalWidth=oToTal.offsetWidth/20;
+		progress = (cuT/toT)*oToTalWidth-0.75;
+		
+		oHadPlay.style.width=progress+'rem';
+		oBar.style.left=progress+'rem';
+		return oHadPlay.style.width,oBar.style.left
+
+	};
+        oBar.addEventListener("touchstart", function(e){
+        	var touchMoveTime=oAudio.currentTime;
+        	var touch = e.targetTouches[0];
+	        startX = touch.pageX;
+	    	function touchMove(e) { //滑动
+		        var toT = oAudio.duration;
+		        var touch = e.targetTouches[0];
+		        var oToTalWidth=oToTal.offsetWidth/20; //总距离
+		        x = (touch.pageX - startX)/20; //滑动的距离
+		        var widthBar =aboveX+ x;
+		        
+		        if(widthBar>16.25){
+		            widthBar=16.25;
+		    	}else{
+		    		if (widthBar<0) {
+		    			widthBar=0
+		    		}
+		    	}			    		
+	    		oBar.style.left = widthBar-0.75 + "rem";
+	           	oHadPlay.style.width=widthBar+'rem';
+		    	//不让进度条超出页面		    	
+		    	time()
+		    	return oBar.style.left
+		    }
+		    function touchEnd(e) { //手指离开屏幕
+		        aboveX = parseInt(oBar.style.left);
+		        var touch = e.targetTouches[0];
+		        var dragPaddingLeft = oBar.style.left;
+		        var changeLeft = dragPaddingLeft.replace("rem", "");
+		        numDragpaddingLeft = changeLeft;
+		        var currentTime = (numDragpaddingLeft / (oToTal.offsetWidth/20-0.75)) * oAudio.duration;//0.75是拖动圆圈的长度，减掉是为了让歌曲结束的时候不会跑到window以外
+		        // console.log(currentTime)
+		        oAudio.currentTime = currentTime;
+		        time();
+		        oBar.removeEventListener('touchmove',touchMove,false)
+		        oBar.removeEventListener('touchend',touchEnd,false)
+		    };
+		    oBar.addEventListener("touchmove", touchMove, false);
+    		oBar.addEventListener("touchend", touchEnd, false);
+    		e.preventDefault();
+    		
+   		}, false);
+        oAudio.addEventListener('ended',function(){
+        	oStartImg.src='images/stop.png';
+        	play=true;
+        },false)
+        oAudio.onloadedmetadata=function(){time()}
+},false)
